718528fe14b5b732e509be04ea1626c655c032d9
AshishAsokan <raashish020@gmail.com>
2023-11-23 01:03:57 +0530

    Modified my website

--- js/macros.js
+++ js/macros.js
@@ -18,7 +18,7 @@ const AD_NUS = "http://news.nus.edu.sg/highlights/model-illuminates-alzheimers-d
 const AD_PT = "https://www.psychologytoday.com/intl/blog/the-athletes-way/201610/alzheimers-study-links-triad-brain-areas-cognition";
 const AD_MGH = "docs/news/ad_mgh.pdf";
 
-const MIT_RED = "#A31F34";
+const MIT_RED = "#135d99";
 const MIT_DARK_GRAY = "#8A8B8C";
 const MIT_LIGHT_GRAY = "#C2C0BF";
 
@@ -31,8 +31,36 @@ function clickable(text, link) {
   document.write("<a class='lowkey' target='_blank' href=" + link + ">" + text + "</a>");
 }
 
+function clickableperson(text, link) {
+  document.write("<a style='color: #135d99;' class='lowkey' target='_blank' href=" + link + ">" + text + "</a>");
+}
+
 function text_button(text, link) {
   clickable("[" + text + "]", link);
+}
+
+function code_link(link) {
+  document.write(`
+  <a href=` + link + ` target='_blank' class="button alt icon fa-github small">Code</a>
+  `)
+}
+
+function paper_link(link) {
+  document.write(`
+  <a href=` + link + ` target='_blank' class="button alt icon fa-file-pdf-o small">Paper</a>
+  `)
+}
+
+function project_link(link) {
+  document.write(`
+  <a href=` + link + ` target='_blank' class="button alt icon fa-file small">Project Page</a>
+  `)
+}
+
+function bib_link(link) {
+  document.write(`
+  <a href=` + link + ` target='_blank' class="button alt icon fa-google small">Bibtex</a>
+  `)
 }
 
 function undef() {
@@ -42,7 +70,22 @@ function undef() {
 function getPerson(id) {
   switch (id) {
     case "me":
-      document.write("<u>Xiuming Zhang</u>");
+      document.write("<u>Ashish Ramayee Asokan</u>");
+      break;
+    case "xiuming":
+      clickable("Xiuming Zhang", "https://xiuming.info/");
+      break;
+    case "harsh":
+      clickable("Harsh Rangwani", "https://rangwani-harsh.github.io/about/");
+      break;
+    case "srava":
+      clickable("Sravanti Addepalli", "https://scholar.google.com/citations?user=MOO12i0AAAAJ&hl=en");
+      break;
+    case "venky":
+      clickable("Prof. Venkatesh Babu", "https://cds.iisc.ac.in/faculty/venky/");
+      break;
+    case "sss":
+      clickable("Prof. Shylaja Sharath", "https://staff.pes.edu/nm1297/");
       break;
     case "freeman_short":
       clickable("Bill Freeman", "https://billf.mit.edu/");
@@ -217,37 +260,49 @@ function getPerson(id) {
   }
 }
 
-function getVenue(id) {
+function getVenue(id, year) {
   switch (id) {
     case "iccv":
-      clickable("ICCV", "https://en.wikipedia.org/wiki/International_Conference_on_Computer_Vision");
+      clickable("ICCV".concat(" ", year), "https://en.wikipedia.org/wiki/International_Conference_on_Computer_Vision");
+      break;
+    case "ijcnn":
+      clickable("WCCI IJCNN".concat(" ", year), "https://ieeexplore.ieee.org/xpl/conhome/1000500/all-proceedings");
+      break;
+    case "aiml":
+      clickable("AI-ML Systems".concat(" ", year), "https://www.aimlsystems.org/2023");
+      break;
+    case "aistats":
+      clickable("AISTATS".concat(" ", year), "https://aistats.org/index.html");
+      break;
+    case "wacv":
+      clickable("WACV".concat(" ", year), "https://www.openresearch.org/wiki/WACV");
       break;
     case "nips":
-      clickable("NeurIPS", "https://en.wikipedia.org/wiki/Conference_on_Neural_Information_Processing_Systems");
+      clickable("NeurIPS".concat(" ", year), "https://en.wikipedia.org/wiki/Conference_on_Neural_Information_Processing_Systems");
       break;
     case "uist":
-      clickable("UIST", "https://en.wikipedia.org/wiki/ACM_Symposium_on_User_Interface_Software_and_Technology");
+      clickable("UIST".concat(" ", year), "https://en.wikipedia.org/wiki/ACM_Symposium_on_User_Interface_Software_and_Technology");
       break;
     case "eccv":
-      clickable("ECCV", "https://en.wikipedia.org/wiki/European_Conference_on_Computer_Vision");
+      clickable("ECCV".concat(" ", year), "https://en.wikipedia.org/wiki/European_Conference_on_Computer_Vision");
       break;
     case "cvpr":
-      clickable("CVPR", "https://en.wikipedia.org/wiki/Conference_on_Computer_Vision_and_Pattern_Recognition");
+      clickable("CVPR".concat(" ", year), "https://en.wikipedia.org/wiki/Conference_on_Computer_Vision_and_Pattern_Recognition");
       break;
     case "tog":
-      clickable("TOG", "https://en.wikipedia.org/wiki/ACM_Transactions_on_Graphics");
+      clickable("TOG".concat(" ", year), "https://en.wikipedia.org/wiki/ACM_Transactions_on_Graphics");
       break;
     case "siggraph":
-      clickable("SIGGRAPH", "https://en.wikipedia.org/wiki/SIGGRAPH");
+      clickable("SIGGRAPH".concat(" ", year), "https://en.wikipedia.org/wiki/SIGGRAPH");
       break;
     case "siggraph_asia":
-      clickable("SIGGRAPH Asia", "https://en.wikipedia.org/wiki/SIGGRAPH");
+      clickable("SIGGRAPH Asia".concat(" ", year), "https://en.wikipedia.org/wiki/SIGGRAPH");
       break;
     case "pnas":
-      clickable("PNAS", "https://en.wikipedia.org/wiki/Proceedings_of_the_National_Academy_of_Sciences_of_the_United_States_of_America");
+      clickable("PNAS".concat(" ", year), "https://en.wikipedia.org/wiki/Proceedings_of_the_National_Academy_of_Sciences_of_the_United_States_of_America");
       break;
     case "ismrm":
-      clickable("ISMRM", "https://en.wikipedia.org/wiki/International_Society_of_Magnetic_Resonance_in_Medicine");
+      clickable("ISMRM".concat(" ", year), "https://en.wikipedia.org/wiki/International_Society_of_Magnetic_Resonance_in_Medicine");
       break;
     default:
       undef();
@@ -256,14 +311,20 @@ function getVenue(id) {
 
 function getAffli(id) {
   switch (id) {
-    case "tesla":
-      clickable("Tesla", "https://www.tesla.com");
+    case "pes":
+      clickable("PES University", "https://pes.edu/");
       break;
-    case "tesla_ap":
-      clickable("Tesla Autopilot", "https://www.tesla.com/autopilot");
+    case "val":
+      clickable("Vision and AI Lab", "https://val.cds.iisc.ac.in/");
       break;
-    case "adobe":
-      clickable("Adobe", "https://www.adobe.com/");
+    case "intel":
+      clickable("Intel", "https://www.intel.in/content/www/in/en/homepage.html");
+      break;
+    case "na":
+      clickable("Niramai Health Analytix", "https://www.niramai.com/");
+      break;
+    case "gmac":
+      clickable("GMAC Intelligence", "https://gi4all.com/");
       break;
     case "csail":
       clickable("Computer Science and Artificial Intelligence Laboratory (CSAIL)", "http://www.csail.mit.edu");
